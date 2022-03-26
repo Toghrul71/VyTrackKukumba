610eb8a9f7330d6f3a14a93d1693f77402080f4f
Toghrul <te7999@gmail.com>
2022-03-26 14:03:25 -0700

    Managers cannot have access to Odometer page test is created

--- src/test/java/com/cydeo/pages/VehicleOdometerPage.java
+++ src/test/java/com/cydeo/pages/VehicleOdometerPage.java
@@ -1,9 +1,15 @@
 package com.cydeo.pages;
 
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class VehicleOdometerPage {
     public VehicleOdometerPage(){
@@ -16,4 +22,18 @@ public class VehicleOdometerPage {
 
     @FindBy(xpath = "//button[@class='btn dropdown-toggle ']")
     public WebElement defaultViewPerPage;
-}
+
+    public void selectModuleAndOption(String module, String option){
+        Actions actions=new Actions(Driver.getDriver());
+        actions.moveToElement(Driver.getDriver().findElement(By.xpath("//span[contains(text( ), "+"'"+module+"')]"))).perform();
+
+        BrowserUtils.sleep(5);
+        WebElement optionElement = Driver.getDriver().findElement(By.xpath("//span[contains(text( ), "+"'"+option+"')]"));
+        optionElement.click();
+
+
+    }
+
+    }
+
+
--- src/test/java/com/cydeo/step_definitions/odometerStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/odometerStepDefinitions.java
@@ -1,12 +1,41 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.LoginPage;
+import com.cydeo.pages.VehicleOdometerPage;
 import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class odometerStepDefinitions {
+    VehicleOdometerPage odometerPage=new VehicleOdometerPage();
+    LoginPage loginPage=new LoginPage();
+
+    @Given("Managers logged in using {string} and {string} to the homepage")
+    public void managers_logged_in_using_and_to_the_homepage(String username, String password) {
+        loginPage.login(username,password);
+    }
+    @Given("Click the {string} option under {string}")
+    public void click_the_option_under_fleet(String option, String module) {
+        WebDriverWait wait=new WebDriverWait(Driver.getDriver(),10);
+        wait.until(ExpectedConditions.titleIs("Dashboard"));
+        odometerPage.selectModuleAndOption(module,option);
+
+    }
+    @Then("Verify managers see {string}")
+    public void verify_managers_see(String expectedMessage) {
+        String actual=Driver.getDriver().findElement(By.xpath("//div[.='You do not have permission to perform this action.']")).getText();
+
+        Assert.assertEquals(expectedMessage,actual);
+    }
 
 }
